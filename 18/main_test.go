f10b0b5e85d458e848cbb9154dddc4f32c73fbf4
Rossiar <rosshenderson92@gmail.com>
2020-12-18 16:00:55 +0000

    feat(18): part two

--- 18/main_test.go
+++ 18/main_test.go
@@ -26,3 +26,28 @@ func Test_parse(t *testing.T) {
 		})
 	}
 }
+
+func Test_parseAdvanced(t *testing.T) {
+	type args struct {
+		expression string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{args: args{expression: "1 + 2 * 3 + 4 * 5 + 6"}, want: 231},
+		{args: args{expression: "1 + (2 * 3) + (4 * (5 + 6))"}, want: 51},
+		{args: args{expression: "2 * 3 + (4 * 5)"}, want: 46},
+		{args: args{expression: "5 + (8 * 3 + 9 + 3 * 4 * 3)"}, want: 1445},
+		{args: args{expression: "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"}, want: 669060},
+		{args: args{expression: "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"}, want: 23340},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseAdvanced(tt.args.expression); got != tt.want {
+				t.Errorf("parseAdvanced() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
